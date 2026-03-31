51450abd20bdfa83bb650d52a687852a05f7450c
Thehara Heshanjali <thehara001@gmail.com>
2026-03-31 11:20:18 +0530

    fix: fix error

--- backend/src/main/java/com/smartcampus/SmartCampusBackendApplication.java
+++ backend/src/main/java/com/smartcampus/SmartCampusBackendApplication.java
@@ -3,7 +3,11 @@ package com.smartcampus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration.class
+})
 public class SmartCampusBackendApplication {
 
 	public static void main(String[] args) {
