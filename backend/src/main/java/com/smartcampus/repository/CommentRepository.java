805bec67e519aabc60525cc8801911e757efc358
Thehara Heshanjali <thehara001@gmail.com>
2026-04-24 02:50:22 +0530

    fix:fix backend-frontend running issue

--- backend/src/main/java/com/smartcampus/repository/CommentRepository.java
+++ backend/src/main/java/com/smartcampus/repository/CommentRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByTicketIdOrderByCreatedAtDesc(Long ticketId);
+    List<Comment> findByUserId(Long userId);
 }
